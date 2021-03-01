3d1b1d13f77dd7eb80ad136b12603d847fba2ed0
hossensumon071 <hossensumon071@gmail.com>
2021-03-01 10:10:01 +0600

    recursion in php

---
+++ 2.7recursion.php
@@ -0,0 +1,28 @@
+<?php
+
+// Recursion
+
+
+function printN($i){
+    if($i>=10){
+        return ;
+    }
+
+    echo $i."\n";
+    $i++;
+    printN($i);
+}
+
+// printN(0);
+
+
+function printNumber($counter, $end, $stepping=1){
+    if($counter>$end){
+        return;
+    }
+    echo $counter."\n";
+    $counter += $stepping;
+    printNumber($counter, $end, $stepping);
+}
+
+printNumber(21, 37, 2);
